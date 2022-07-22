687a377f2f29707a493627570f56083e4d27a25c
Артём Журавлёв <artemon50309@gmail.com>
2022-07-22 16:27:48 +0200

    initial commit

---
+++ src/main/java/org/example/IntsCalculator.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class IntsCalculator implements Ints {
+    protected final Calculator target;
+
+    public IntsCalculator() {
+        this.target = new Calculator();
+    }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        double initial = target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM)
+                .result();
+        int result = (int) initial;
+        return result;
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        double initial = target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result();
+        int result = (int) initial;
+        return result;
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        double initial = target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.POW)
+                .result();
+        int result = (int) initial;
+        return result;
+    }
+}
